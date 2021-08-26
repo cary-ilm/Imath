de814d98066389cd3a443eae758ef0fcdfca304a
Cary Phillips <cary@ilm.com>
2021-08-26 16:30:43 -0700

    namespace
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- src/Imath/ImathVec.h
+++ src/Imath/ImathVec.h
@@ -3,35 +3,27 @@
 // Copyright Contributors to the OpenEXR Project.
 // 
 
-///
-/// 4-element vector
-///
-
+namespace Imath {
+    
 template <class T> class Vec4
 {
   public:
 
-    T x;
-
-    /// Copy constructor
-     constexpr Vec4 (const Vec4& v) noexcept;
-
-    /// Construct from Vec4 of another base type
+    constexpr Vec4 (const Vec4& v) noexcept;
     template <class S>  constexpr Vec4 (const Vec4<S>& v) noexcept;
 };
 
-template <class T>  constexpr inline Vec4<T>::Vec4 (const Vec4& v) noexcept
-    : x(v.x)
+template <class T>
+constexpr inline Vec4<T>::Vec4 (const Vec4& v) noexcept
 {
 }
 
 template <class T> template <class S>
- constexpr inline Vec4<T>::Vec4 (const Vec4<S>& v) noexcept
-    : x(T(v.x))
+constexpr inline Vec4<T>::Vec4 (const Vec4<S>& v) noexcept
 {
 }
 
-
+}
 
 
 
