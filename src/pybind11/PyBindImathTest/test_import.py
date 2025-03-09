983b20a143c271ab542d543ef6258ea47d25a2f9
Cary Phillips <cary@ilm.com>
2025-03-09 11:31:11 -0700

    path
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- src/pybind11/PyBindImathTest/test_import.py
+++ src/pybind11/PyBindImathTest/test_import.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
+import os
+
+def print_pythonpath_files():
+    pythonpath = os.getenv("PYTHONPATH")
+    if not pythonpath:
+        print("PYTHONPATH is not set.")
+        return
+
+    print(f"PYTHONPATH:")
+    for path in pythonpath.split(os.pathsep):
+        if not os.path.isdir(path):
+            continue  # Skip if not a valid directory
+
+        for root, _, files in os.walk(path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                print(f"    {file_path}")
+
 def test_import():
+    
+    print_pythonpath_files()
+
     try:
         import pybindimath
         print()
         print(f"pybindimath imported from {pybindimath.__file__}")
         print("ok")
-    except:
-        print(f"test_import.py failed. Can't import pybindimath")
+    except Exception as e:
+        print(f"test_import.py failed. Can't import pybindimathL {e}")
         print("meh")
         
 test_import()
