55ce1d8d384dc76c1d3e198f9f49acee9cd80d6b
Cary Phillips <cary@ilm.com>
2025-04-05 15:49:20 -0700

    ci1
    Signed-off-by: Cary Phillips <cary@ilm.com>

---
+++ src/pybind11/PyBindImathTest/test_import_pybindimath.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os, sys, site, glob
+
+def test_import():
+
+    print(f"test_import: sys.path={sys.path}")
+    print(f"PYTHONPATH: {os.environ['PYTHONPATH']}")
+    print(f"cwd: {os.getcwd()}")
+    print(f"looking for pybindimath...")
+    found = False
+    for root, _, files in os.walk(os.getcwd()):
+        for file in files:
+            if file.startswith("pybindimath.") and file.endswith((".so",".pyd")):
+                print(f"  found: {os.path.join(root, file)}")
+                found = True
+    if not found:
+        print("not found.")
+
+    try:
+        import pybindimath
+        print()
+        print(f"pybindimath imported from {pybindimath.__file__}")
+        print("ok")
+    except Exception as e:
+        print(f"test_import.py failed. Can't import pybindimath: {e}")
+        print("meh")
+
+test_import()
