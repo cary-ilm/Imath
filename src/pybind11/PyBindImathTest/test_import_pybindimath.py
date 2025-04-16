70f212387efdef3a7f5529cf6a82fecee2692f9f
Cary Phillips <cary@ilm.com>
2025-04-15 20:50:46 -0700

    fix16
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- src/pybind11/PyBindImathTest/test_import_pybindimath.py
+++ src/pybind11/PyBindImathTest/test_import_pybindimath.py
@@ -2,6 +2,44 @@
 
 import os, sys, site, glob
 
+print(f"{__file__}: PYTHONPATH={os.environ['PYTHONPATH']}")
+pythonpath = os.environ.get('PYTHONPATH')
+if pythonpath:
+    # Use os.pathsep which is ':' on Unix/macOS and ';' on Windows
+    paths = pythonpath.split(os.pathsep)
+    for path in paths:
+        if not path:
+            continue
+        resolved_path = Path(path).expanduser().resolve()
+        print(f"Contents of: {resolved_path}")
+        if resolved_path.is_dir():
+            for root, dirs, files in os.walk(resolved_path):
+                for f in files:
+                    file_path = Path(root) / f
+                    print(f"  {file_path}")
+        else:
+            print(f"  [Not a directory or does not exist]")
+        print()
+PATH = os.environ.get('PATH')
+if PATH:
+    print("PATH={PATH}")
+    # Use os.pathsep which is ':' on Unix/macOS and ';' on Windows
+    paths = PATH.split(os.pathsep)
+    for path in paths:
+        if not path:
+            continue
+        resolved_path = Path(path).expanduser().resolve()
+        print(f"Contents of: {resolved_path}")
+        if resolved_path.is_dir():
+            for root, dirs, files in os.walk(resolved_path):
+                for f in files:
+                    file_path = Path(root) / f
+                    print(f"  {file_path}")
+        else:
+            print(f"  [Not a directory or does not exist]")
+        print()
+
+
 def test_import():
 
     try:
