49ac6396929692b91930d879ef46d56e59dd8df0
Cary Phillips <cary@ilm.com>
2025-04-20 16:29:09 -0700

    sun2
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- src/python/PyImathTest/test_import_imath.py
+++ src/python/PyImathTest/test_import_imath.py
@@ -1,37 +1,19 @@
 #!/usr/bin/env python3
 
-import os, site, sys, glob
+import os, sys, site, glob
+from pathlib import Path
 
-def list_files_in_path(var):
+def print_files_in_path(var):
     path_env = os.environ.get(var, '')
-    path_dirs = path_env.split(os.pathsep)
-    all_files = []
-    for directory in path_dirs:
-        if not directory:
-            continue
-        directory = os.path.normpath(directory)
-        if os.path.isdir(directory):
-            try:
-                for entry in os.listdir(directory):
-                    full_path = os.path.join(directory, entry)
+    print(f"{var}={path_env}")
+    for directory in path_env.split(os.pathsep):
+        if directory:
+            directory = os.path.normpath(directory)
+            if os.path.isdir(directory):
+                for file in os.listdir(directory):
+                    full_path = os.path.join(directory, file)
                     if os.path.isfile(full_path):
-                        all_files.append(full_path)
-            except PermissionError:
-                print(f"Permission denied accessing: {directory}")
-            except OSError as e:
-                print(f"Error accessing {directory}: {e}")
-    
-    return path_env, all_files
-
-path_env, path_files = list_files_in_path('PATH')
-print(f"{__file__}: PATH={path_env}")
-for f in path_files:
-    print(f"  {f}")
-pythonpath_env, pythonpath_files = list_files_in_path('PYTHONPATH')
-print(f"PYTHONPATH={pythonpath_env}")
-for f in pythonpath_files:
-    print(f"  {f}")
-
+                        print(f'    {full_path}')
 
 def test_import():
 
@@ -41,7 +23,9 @@ def test_import():
         print(f"imath imported from {imath.__file__}")
         print("ok")
     except Exception as e:
-        print(f"test_import_imath.py failed. Can't import imath: {e}")
-        print("meh")
+        print(f"{__file__} failed. Can't import imath: {e}")
+        print_files_in_path('PATH')
+        print_files_in_path('PYTHONPATH')
+        sys.exit(1)
 
 test_import()
