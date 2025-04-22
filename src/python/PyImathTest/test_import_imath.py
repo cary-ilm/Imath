4f8d5134f92da18d5ce78d89fff690d00961196b
Cary Phillips <cary@ilm.com>
2025-04-21 18:32:43 -0700

    mon13
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- src/python/PyImathTest/test_import_imath.py
+++ src/python/PyImathTest/test_import_imath.py
@@ -24,17 +24,22 @@ def test_import():
         print("ok")
     except Exception as e:
         print(f"{__file__} failed. Can't import imath: {e}")
-        print_files_in_path('PATH')
-        print_files_in_path('PYTHONPATH')
+        #print_files_in_path('PATH')
+        #print_files_in_path('PYTHONPATH')
 
         import traceback
         traceback.print_exc()
         
         import ctypes
         try:
-            lib = ctypes.cdll.LoadLibrary("/d/a/Imath/Imath/_install/python/imath.pyd")
+            print('ctypes.cdll.LoadLibrary("/a/Imath/Imath/_install/python/imath.pyd")')
+
+            lib = ctypes.cdll.LoadLibrary("/a/Imath/Imath/_install/python/imath.pyd")
+
+            print(f"lib={lib}")
+            
         except Exception as e:
-            print(f"Error loading library: {e}")
+            print(f"Error in cdll.LoadLibrary: {e}")
 
         sys.exit(1)
 
