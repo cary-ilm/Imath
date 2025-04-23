6abeb206075f1f8497584702d3d18be3a1a42f00
Cary Phillips <cary@ilm.com>
2025-04-23 16:59:28 -0700

    wed7
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- src/pybind11/PyBindImath/PyBindImath.h
+++ src/pybind11/PyBindImath/PyBindImath.h
@@ -10,17 +10,21 @@
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
 #include <pybind11/operators.h>
-//#include "PyBindImathExport.h"
+#include "PyBindImathExport.h"
 
 #ifndef _PyBindImath_h_
 #define _PyBindImath_h_
 
 namespace PyBindImath {
+
 PYBINDIMATH_EXPORT void register_imath_fun(pybind11::module& m);
+
 PYBINDIMATH_EXPORT void register_imath_vec(pybind11::module& m);
 PYBINDIMATH_EXPORT void register_imath_box(pybind11::module& m);
 PYBINDIMATH_EXPORT void register_imath_plane(pybind11::module& m);
 PYBINDIMATH_EXPORT void register_imath_line(pybind11::module& m);
+// PYBINDIMATH_EXPORT void register_imath_euler(pybind11::module& m)
+// PYBINDIMATH_EXPORT void register_imath_frustum(pybind11::module& m)
 }
 
 #endif
